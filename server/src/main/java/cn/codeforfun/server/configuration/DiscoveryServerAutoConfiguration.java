89bbc0dca0973eb20baa4e2659ebedaaf9110f2e
王斌 <253498229@qq.com>
2021-09-13 17:03:08 +0800

    review

--- server/src/main/java/cn/codeforfun/server/configuration/DiscoveryServerAutoConfiguration.java
+++ server/src/main/java/cn/codeforfun/server/configuration/DiscoveryServerAutoConfiguration.java
@@ -1,8 +1,15 @@
 package cn.codeforfun.server.configuration;
 
+import cn.hutool.core.net.NetUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DiscoveryServerAutoConfiguration {
+    @Bean
+    public void test() {
+        String localhostIp = NetUtil.getLocalhostStr();
+        System.out.println(localhostIp);
+    }
 
 }
