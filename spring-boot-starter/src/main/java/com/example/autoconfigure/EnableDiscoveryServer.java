2bf5c323c24ad63f5d924123bb22b88d4d3b9a2c
王斌 <253498229@qq.com>
2021-09-13 14:37:25 +0800

    review

--- spring-boot-starter/src/main/java/com/example/autoconfigure/EnableDiscoveryServer.java
+++
@@ -1,9 +0,0 @@
-package com.example.autoconfigure;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface EnableDiscoveryServer {
-}
