f27001fbdb502c32e7fe3447b79c6cdbfed2c854
王斌 <253498229@qq.com>
2021-09-14 17:31:24 +0800

    review

--- server-implement/server-mysql/src/main/java/cn/codeforfun/server/configuration/MySQLConfiguration.java
+++ server-implement/server-mysql/src/main/java/cn/codeforfun/server/configuration/MySQLConfiguration.java
@@ -2,12 +2,14 @@ package cn.codeforfun.server.configuration;
 
 import cn.codeforfun.server.data.DataHandler;
 import cn.codeforfun.server.data.MySQLHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MySQLConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     public DataHandler dataHandler() {
         return new MySQLHandler();
     }
