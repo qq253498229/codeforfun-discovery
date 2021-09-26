2f440a01bd140116efdb1a2b3cfba6d92bdc034a
王斌 <253498229@qq.com>
2021-09-26 15:07:32 +0800

    update

--- client-implement/client-mysql/sql/schema.sql
+++ client-implement/client-mysql/sql/schema.sql
@@ -1,7 +1,10 @@
 create table if not exists cff_service
 (
     id          bigint auto_increment primary key,
-    name        varchar(200) comment '服务名',
+    name        varchar(50) comment '服务名',
     remark      varchar(500) comment '备注',
-    last_active datetime comment '上次活跃时间'
+    host        varchar(100) comment '地址',
+    port        int comment '端口',
+    last_active datetime comment '上次活跃时间',
+    unique key `unique_service` (`host`, `port`)
 );
