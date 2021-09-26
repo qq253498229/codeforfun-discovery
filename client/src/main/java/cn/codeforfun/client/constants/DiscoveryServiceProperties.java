c50ea3c214ca45265fac5c6961372134f122f6e9
王斌 <253498229@qq.com>
2021-09-26 16:33:54 +0800

    review

--- client/src/main/java/cn/codeforfun/client/constants/DiscoveryServiceProperties.java
+++ client/src/main/java/cn/codeforfun/client/constants/DiscoveryServiceProperties.java
@@ -9,30 +9,30 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "discovery.service")
 public class DiscoveryServiceProperties {
     /**
-     * 服务激活间隔。单位: 秒
+     * 服务激活间隔,单位:秒
      * 每隔多长时间激活一次自身服务
      */
     private Integer serviceActiveInterval = 10;
     /**
-     * 刷新服务列表间隔。单位: 秒
+     * 刷新服务列表间隔,单位:秒
      * 每隔多长时间刷新一次服务列表
      */
     private Integer refreshServiceListInterval = 20;
     /**
-     * 服务激活超时时间。单位: 秒
-     * 超过这个时间后该服务就会被认为是有故障的，之后就不会调用这个服务
+     * 服务激活超时时间,单位:秒
+     * 超过这个时间后该服务就会被认为是有故障的,之后就不会调用这个服务
      */
     private Integer serviceActiveTimeout = 60;
     /**
-     * 服务名。如果没设置的话会取 (spring.application.name) 的值
+     * 服务名,如果没设置的话会取(spring.application.name)的值
      */
     private String name;
     /**
-     * 服务端口。如果没设置的话会取 (server.port) 的值，如果还是没有则默认8080
+     * 服务端口,如果没设置的话会取(server.port)的值,如果还是没有则默认8080
      */
     private Integer port;
     /**
-     * 服务地址。如果没设置会取本地ip
+     * 服务地址,如果没设置会取本地ip
      */
     private String host;
 }
