3dd162596fb78e27dcaf65b4738e3bb044759213
王斌 <253498229@qq.com>
2021-09-27 14:41:13 +0800

    review

--- client-implement/client-mysql/src/main/java/cn/codeforfun/client/data/MySQLHandler.java
+++ client-implement/client-mysql/src/main/java/cn/codeforfun/client/data/MySQLHandler.java
@@ -15,13 +15,15 @@ public class MySQLHandler implements DataHandler {
 
     @Override
     public void registerService(ServiceInstance serviceInstance) {
-        String sql = "insert into cff_service (name, host, port, last_active) value (?, ?, ?, now()) " +
-                "on duplicate key update last_active=now()";
-        jdbcTemplate.update(sql, serviceInstance.getName(), serviceInstance.getHost(), serviceInstance.getPort());
+        activeCurrentService(serviceInstance);
     }
 
     @Override
     public void activeService(ServiceInstance serviceInstance) {
+        activeCurrentService(serviceInstance);
+    }
+
+    private void activeCurrentService(ServiceInstance serviceInstance) {
         String sql = "insert into cff_service (name, host, port, last_active) value (?, ?, ?, now()) " +
                 "on duplicate key update last_active=now()";
         jdbcTemplate.update(sql, serviceInstance.getName(), serviceInstance.getHost(), serviceInstance.getPort());
