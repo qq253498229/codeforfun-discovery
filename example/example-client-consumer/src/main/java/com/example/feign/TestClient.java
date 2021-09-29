eade75e43a4a718332382eb29b331b753adc66c0
王斌 <253498229@qq.com>
2021-09-29 09:31:07 +0800

    添加断路器

--- example/example-client-consumer/src/main/java/com/example/feign/TestClient.java
+++ example/example-client-consumer/src/main/java/com/example/feign/TestClient.java
@@ -3,7 +3,7 @@ package com.example.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("client-producer")
+@FeignClient(value = "client-producer", fallback = TestClientFallback.class)
 public interface TestClient {
     @GetMapping("/test1")
     String test1();
